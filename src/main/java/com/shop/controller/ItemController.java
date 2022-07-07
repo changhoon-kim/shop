ae9a29c7d5ba1c725eb44e4793d9a5dcf2ed3a7c
changhoon-kim <changhoon.kim@worksmobile.com>
2022-07-07 11:38:48 +0900

    item registry2

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -2,12 +2,14 @@ package com.shop.controller;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -43,6 +45,40 @@ public class ItemController {
 			itemService.saveItem(itemFormDto, itemImgFileList);
 		} catch (Exception e) {
 			model.addAttribute("errorMessage", "상품 등록 중 에러가 발생했습니다.");
+			return "item/itemForm";
+		}
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping(value="/admin/item/{itemId}")
+	public String itemDtl(@PathVariable("itemId") Long itemId, Model model) {
+		try {
+			ItemFormDto itemFormDto = itemService.getItemDetl(itemId);
+			model.addAttribute("itemFormDto", itemFormDto);
+		} catch (EntityNotFoundException e) {
+			model.addAttribute("errorMessage", "존재하지 않는 상품입니다.");
+			model.addAttribute("itemFormDto", new ItemFormDto());
+		}
+		
+		return "item/itemForm";
+	}
+	
+	@PostMapping(value="/admin/item/{itemId}")
+	public String itemUpdate(@Valid ItemFormDto itemFormDto, BindingResult bindingResult, @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList, Model model) {
+		if (bindingResult.hasErrors()) {
+			return "item/itemForm";
+		}
+		
+		if (itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null) {
+			model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값입니다.");
+			return "item/itemForm";
+		}
+		
+		try {
+			itemService.updateItem(itemFormDto, itemImgFileList);
+		} catch (Exception e) {
+			model.addAttribute("errorMessage", "상품 수정 중 에러가 발생했습니다.");
 			return "item/itemForm";
 		}
 		
--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -11,6 +11,7 @@ import javax.persistence.Lob;
 import javax.persistence.Table;
 
 import com.shop.constant.ItemSellStatus;
+import com.shop.dto.ItemFormDto;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -42,4 +43,12 @@ public class Item extends BaseEntity {
 	
 	@Enumerated(EnumType.STRING)
 	private ItemSellStatus itemSellStatus;
+	
+	public void updateItem(ItemFormDto itemFormDto) {
+		this.itemNm = itemFormDto.getItemNm();
+		this.price = itemFormDto.getPrice();
+		this.stockNumber = itemFormDto.getStockNumber();
+		this.itemDetail = itemFormDto.getItemDetail();
+		this.itemSellStatus = itemFormDto.getItemSellStatus();
+	}
 }
